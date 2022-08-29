efeb9ccc4193a096578b38a645726c6404fbd3c9
unknown <lixy0311@163.com>
2022-08-29 13:11:27 +0800

    carousel upload

--- frontend/src/views/Home/Main/Welcome.vue
+++ frontend/src/views/Home/Main/Welcome.vue
@@ -1,23 +1,25 @@
 <template>
 <div>
- <el-carousel :interval="4000" type="card" height="540px">
+ <el-carousel :interval="4000" type="card" height="400px">
         <el-carousel-item>
-          <img style="width: 960px; height: 540px" src="@/assets/pics/p1.png">
+        <router-link :to="{ path: '/attack' }">
+          <img style="width: 640px; height: 400px" src="@/assets/pics/p1.png">
+        </router-link>
         </el-carousel-item>
         <el-carousel-item>
-          <img style="width: 960px; height: 540px" src="@/assets/pics/p2.png">
+        <router-link :to="{ path: '/verify' }">
+          <img style="width: 640px; height: 400px" src="@/assets/pics/p2.png">
+        </router-link>
         </el-carousel-item>
         <el-carousel-item>
-          <img style="width: 960px; height: 540px" src="@/assets/pics/p3.png">
+        <router-link :to="{ path: '/repair' }">
+          <img style="width: 640px; height: 400px" src="@/assets/pics/text_attack.jpg">
+        </router-link>
         </el-carousel-item>
         <el-carousel-item>
-          <img style="width: 960px; height: 540px" src="@/assets/pics/p4.png">
-        </el-carousel-item>
-        <el-carousel-item>
-          <img style="width: 960px; height: 540px" src="@/assets/pics/p5.png">
-        </el-carousel-item>
-        <el-carousel-item>
-          <img style="width: 960px; height: 540px" src="@/assets/pics/p6.png">
+        <router-link :to="{ path: '/project' }">
+          <img style="width: 640px; height: 400px" src="@/assets/pics/verification.png">
+        </router-link>
         </el-carousel-item>
    </el-carousel>
    <Guide></Guide>
